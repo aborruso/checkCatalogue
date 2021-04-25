ef5aa62714cdfd1016722413dd4dcbc7722190d1
Andrea Borruso <aborruso@gmail.com>
2021-04-25 23:50:41 +0200

    rimosso riferimenti assoluti dal path

--- script/report.sh
+++ script/report.sh
@@ -40,17 +40,17 @@ cat <<-EOF >"$folder"/../output/"$name"/processing/report/"$name".md
 EOF
 
 # aggiungi riepilogo
-mlr --c2m count-distinct -f httpReply "$folder"/../output/openDataComunePalermo/processing/httpReply.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
+mlr --c2m count-distinct -f httpReply "$folder"/../output/"$name"/processing/httpReply.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
 
 # aggiungi link a file dettagli
 
-mlr --n2c --ifs '\t' label id,downloadURL "$folder"/../output/openDataComunePalermo/rawdata/distributionsCSV.tsv >"$folder"/../output/"$name"/rawdata/tmp.csv
+mlr --n2c --ifs '\t' label id,downloadURL "$folder"/../output/"$name"/rawdata/distributionsCSV.tsv >"$folder"/../output/"$name"/rawdata/tmp.csv
 
-mlr --csv join --ul -j "dcat:downloadURL:@rdf:resource" -l "dcat:downloadURL:@rdf:resource" -r downloadURL -f "$folder"/../output/openDataComunePalermo/rawdata/distributionsCSV.csv then unsparsify then reorder -f id "$folder"/../output/openDataComunePalermo/rawdata/tmp.csv >"$folder"/../output/"$name"/processing/report/HTTPreport.csv
+mlr --csv join --ul -j "dcat:downloadURL:@rdf:resource" -l "dcat:downloadURL:@rdf:resource" -r downloadURL -f "$folder"/../output/"$name"/rawdata/distributionsCSV.csv then unsparsify then reorder -f id "$folder"/../output/"$name"/rawdata/tmp.csv >"$folder"/../output/"$name"/processing/report/HTTPreport.csv
 
-mlr --csv join --ul -j id -l id -r file -f "$folder"/../output/openDataComunePalermo/processing/report/HTTPreport.csv then unsparsify then sort -n id "$folder"/../output/openDataComunePalermo/processing/httpReply.csv | sponge "$folder"/../output/openDataComunePalermo/processing/report/HTTPreport.csv
+mlr --csv join --ul -j id -l id -r file -f "$folder"/../output/"$name"/processing/report/HTTPreport.csv then unsparsify then sort -n id "$folder"/../output/"$name"/processing/httpReply.csv | sponge "$folder"/../output/"$name"/processing/report/HTTPreport.csv
 
-mlr -I reorder -f id,httpReply "$folder"/../output/openDataComunePalermo/processing/report/HTTPreport.csv
+mlr -I --csv reorder -f id,httpReply "$folder"/../output/"$name"/processing/report/HTTPreport.csv
 
 # titolo sezione
 cat <<-EOF >>"$folder"/../output/"$name"/processing/report/"$name".md
@@ -85,7 +85,7 @@ mlr <"$folder"/../output/"$name"/processing/distributionsFormat.json --j2m cat >
 numeroDataset=$(wc <"$folder"/../output/"$name"/rawdata/dataset.jsonl -l)
 numeroRisorse=$(wc <"$folder"/../output/"$name"/rawdata/distributions.jsonl -l)
 numeroCSV=$(mlr --j2n filter '$format=="CSV"' then cut -f count "$folder"/../output/"$name"/processing/distributionsFormat.json)
-numeroFileErrore=$(mlr --c2n count -n -g file "$folder"/../output/openDataComunePalermo/processing/errors.csv)
+numeroFileErrore=$(mlr --c2n count -n -g file "$folder"/../output/"$name"/processing/errors.csv)
 
 cat <<-EOF >>"$folder"/../output/"$name"/processing/report/"$name".md
 
@@ -103,9 +103,9 @@ mlr <"$folder"/../output/"$name"/rawdata/distributions.jsonl --j2m unsparsify th
 
 ### errori ###
 
-mlr --csv join --ul -j "dcat:downloadURL:@rdf:resource" -l "dcat:downloadURL:@rdf:resource" -r downloadURL -f "$folder"/../output/openDataComunePalermo/rawdata/distributionsCSV.csv then unsparsify then reorder -f id then put -S '$id=$id.".csv"' "$folder"/../output/openDataComunePalermo/rawdata/tmp.csv >"$folder"/../output/"$name"/processing/report/errorsReport.csv
+mlr --csv join --ul -j "dcat:downloadURL:@rdf:resource" -l "dcat:downloadURL:@rdf:resource" -r downloadURL -f "$folder"/../output/"$name"/rawdata/distributionsCSV.csv then unsparsify then reorder -f id then put -S '$id=$id.".csv"' "$folder"/../output/"$name"/rawdata/tmp.csv >"$folder"/../output/"$name"/processing/report/errorsReport.csv
 
-mlr --csv join --ul -j id -l id -r file -f "$folder"/../output/"$name"/processing/report/errorsReport.csv then unsparsify "$folder"/../output/openDataComunePalermo/processing/errors_wide.csv | sponge "$folder"/../output/"$name"/processing/report/errorsReport.csv
+mlr --csv join --ul -j id -l id -r file -f "$folder"/../output/"$name"/processing/report/errorsReport.csv then unsparsify "$folder"/../output/"$name"/processing/errors_wide.csv | sponge "$folder"/../output/"$name"/processing/report/errorsReport.csv
 
 mlr -I --csv put -S '$id=sub($id,"\..+","")' then sort -n id "$folder"/../output/"$name"/processing/report/errorsReport.csv
 
@@ -129,7 +129,7 @@ A seguire uno spaccato su:
 
 EOF
 
-mlr --c2m filter -x -S '$encoding==""' then cut  -f bytes,fields,rows then stats1 -a min,max,mean,p25,p50,p75 -f bytes,fields,rows then reshape -r  '_' -o item,value then nest --explode --values --across-fields -f item --nested-fs "_" then reshape -s item_2,value then rename item_1,property ../output/openDataComunePalermo/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
+mlr --c2m filter -x -S '$encoding==""' then cut  -f bytes,fields,rows then stats1 -a min,max,mean,p25,p50,p75 -f bytes,fields,rows then reshape -r  '_' -o item,value then nest --explode --values --across-fields -f item --nested-fs "_" then reshape -s item_2,value then rename item_1,property ../output/"$name"/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
 
 cat <<-EOF >>"$folder"/../output/"$name"/processing/report/"$name".md
 
@@ -139,7 +139,7 @@ Questo l'*encoding* delle risorse CSV del catalogo.
 
 EOF
 
-mlr --c2m filter -x -S '$encoding==""' then count-distinct -f encoding "$folder"/../output/openDataComunePalermo/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
+mlr --c2m filter -x -S '$encoding==""' then count-distinct -f encoding "$folder"/../output/"$name"/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
 
 cat <<-EOF >>"$folder"/../output/"$name"/processing/report/"$name".md
 
@@ -149,13 +149,13 @@ Questi i separatori di campo delle risorse CSV del catalogo.
 
 EOF
 
-mlr --c2m filter -x -S '$encoding==""' then count-distinct -f delimiter "$folder"/../output/openDataComunePalermo/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
+mlr --c2m filter -x -S '$encoding==""' then count-distinct -f delimiter "$folder"/../output/"$name"/processing/validate.csv >>"$folder"/../output/"$name"/processing/report/"$name".md
 
 ### anagrafica di base
 
-mlr --csv join --ul -j file -l file -r id -f "$folder"/../output/"$name"/processing/validate.csv then unsparsify  "$folder"/../output/openDataComunePalermo/rawdata/tmp.csv >"$folder"/../output/openDataComunePalermo/rawdata/tmp_validate.csv
+mlr --csv join --ul -j file -l file -r id -f "$folder"/../output/"$name"/processing/validate.csv then unsparsify  "$folder"/../output/"$name"/rawdata/tmp.csv >"$folder"/../output/"$name"/rawdata/tmp_validate.csv
 
-mlr --csv join --ul -j downloadURL -l downloadURL -r "dcat:downloadURL:@rdf:resource" -f "$folder"/../output/openDataComunePalermo/rawdata/tmp_validate.csv then unsparsify then reorder -e -f downloadURL "$folder"/../output/openDataComunePalermo/rawdata/distributionsCSV.csv >"$folder"/../output/"$name"/processing/report/anagrafica.csv
+mlr --csv join --ul -j downloadURL -l downloadURL -r "dcat:downloadURL:@rdf:resource" -f "$folder"/../output/"$name"/rawdata/tmp_validate.csv then unsparsify then reorder -e -f downloadURL "$folder"/../output/"$name"/rawdata/distributionsCSV.csv >"$folder"/../output/"$name"/processing/report/anagrafica.csv
 
 cat <<-EOF >>"$folder"/../output/"$name"/processing/report/"$name".md
 
